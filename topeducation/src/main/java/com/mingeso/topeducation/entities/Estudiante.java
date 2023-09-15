5ec8f919673d28a087ea9b59a770d75d3c2f5496
Héctor Gallardo <hector.gallardo.a@usach.cl>
2023-09-15 17:10:01 -0300

    entidad estudiante y dump base de datos listo

---
+++ topeducation/src/main/java/com/mingeso/topeducation/entities/Estudiante.java
@@ -0,0 +1,40 @@
+package com.mingeso.topeducation.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.time.Year;
+
+@Entity
+@Table(name = "estudiante")
+@Data
+@NoArgsConstructor
+public class Estudiante {
+    @Id
+    Integer id;
+    @Column(name = "rut")
+    String rut;
+    @Column(name = "nombre1")
+    String nombre1;
+    @Column(name = "nombre2")
+    String nombre2;
+    @Column(name = "apellido1")
+    String apellido1;
+    @Column(name = "apellido2")
+    String apellido2;
+    @Column(name = "fecha_nacimiento")
+    Date fechaNacimiento;
+    @Column(name = "anio_egreso")
+    Year anioEgreso;
+    @Column(name = "nombre_colegio")
+    String nombreColegio;
+    String tipoColegio;
+    String tipoPagoArancel;
+    Integer interes;
+}
