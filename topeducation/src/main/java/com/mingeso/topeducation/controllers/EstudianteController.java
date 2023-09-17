a641795ac043d7b75ebc94ea2ebd0b1a27a110ab
Héctor Gallardo <hector.gallardo.a@usach.cl>
2023-09-17 02:35:39 -0300

    HU-2 ingresar estudiante por pantalla, faltaría agregar excepciones si queda tiempo y estilizar un poco sin tailwind (porque no funciona muy bien al parecer por cdn)

--- topeducation/src/main/java/com/mingeso/topeducation/controllers/EstudianteController.java
+++ topeducation/src/main/java/com/mingeso/topeducation/controllers/EstudianteController.java
@@ -1,22 +1,41 @@
 package com.mingeso.topeducation.controllers;
 
 import com.mingeso.topeducation.requests.SaveEstudianteRequest;
+import com.mingeso.topeducation.responses.ApiResponse;
 import com.mingeso.topeducation.services.EstudianteService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/estudiantes")
 @RequiredArgsConstructor
 public class EstudianteController {
     private final EstudianteService estudianteService;
 
-    @PostMapping("/crear")
-    public ResponseEntity<?> saveEstudiante(@RequestBody SaveEstudianteRequest request){
+    @GetMapping("/ingresar")
+    public String ingresar(){
+        return "ingresar-estudiante.html";
+    }
+
+    @GetMapping("/exito")
+    public String exito(){
+        return "exito.html";
+    }
+
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.SEE_OTHER)
+    public ResponseEntity<?> saveEstudiante(@RequestBody SaveEstudianteRequest request) {
         estudianteService.saveEstudiante(request);
-        return new ResponseEntity<>("Estudiante ingresado", HttpStatus.OK);
+        ApiResponse response = new ApiResponse(HttpStatus.CREATED.value(),
+                "Estudiante ingresado correctamente!",
+                "/estudiantes/exito");
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 }
--- topeducation/src/main/java/com/mingeso/topeducation/responses/ApiResponse.java
+++ topeducation/src/main/java/com/mingeso/topeducation/responses/ApiResponse.java
@@ -1,12 +1,21 @@
 package com.mingeso.topeducation.responses;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse {
-    int status;
+    Integer status;
     String message;
+    String pathRedirect;
     Object data;
+
+    public ApiResponse(Integer status, String message, String urlRedirect){
+        this.status = status;
+        this.message = message;
+        this.pathRedirect = urlRedirect;
+    }
 }
