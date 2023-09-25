746a7f018f4f88d64edc3d8bd25fca0d9edb041c
hector-gallard0 <hector.gallardo.a@usach.cl>
2023-09-24 22:35:25 -0400

    backup bdd

--- topeducation/dump.sql
+++ topeducation/dump.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.1.0, for Win64 (x86_64)
+g-- MySQL dump 10.13  Distrib 8.1.0, for Win64 (x86_64)
 --
 -- Host: localhost    Database: topeducation
 -- ------------------------------------------------------
@@ -514,4 +514,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-24 22:30:44
+-- Dump completed on 2023-09-24 22:35:10
