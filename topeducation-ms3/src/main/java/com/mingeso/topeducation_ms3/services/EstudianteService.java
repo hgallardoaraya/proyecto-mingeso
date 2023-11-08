c2f2aa3708b3df2d551e2b0434981c08bf160d96
Héctor Gallardo <hector.gallardo.a@usach.cl>
2023-11-08 12:24:45 -0300

    microservicios con api gateway, service discovery y service config listos

---
+++ topeducation-ms3/src/main/java/com/mingeso/topeducation_ms3/services/EstudianteService.java
@@ -0,0 +1,42 @@
+package com.mingeso.topeducation_ms3.services;
+
+import com.mingeso.topeducation_ms3.dtos.estudiante.EstudianteDTO;
+import com.mingeso.topeducation_ms3.dtos.estudiante.EstudiantesResponse;
+import com.mingeso.topeducation_ms3.exceptions.ApiErrorException;
+import com.mingeso.topeducation_ms3.exceptions.RegistroNoExisteException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.List;
+
+@Service
+public class EstudianteService {
+    WebClient webClient;
+
+    public EstudianteService(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
+    }
+
+    public List<EstudianteDTO> obtenerEstudiantes(){
+        EstudiantesResponse response = webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/estudiantes")
+                        .build())
+                .retrieve()
+                .bodyToMono(EstudiantesResponse.class)
+                .onErrorResume(WebClientResponseException.class, ex -> {
+                    if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+                        throw new RegistroNoExisteException("Estudiantes no encontrados");
+                    } else {
+                        throw new ApiErrorException("Error al obtener a los estudiantes.");
+                    }
+                })
+                .block();
+        if(response == null) throw new ApiErrorException("Error al obtener a los estudiantes.");
+        System.out.println("Respuesta ms1 " + response.getEstudiantes());
+        return response.getEstudiantes();
+    }
+}
---
+++ topeducation-ms3/src/main/java/com/mingeso/topeducation_ms3/services/PagoService.java
@@ -0,0 +1,40 @@
+package com.mingeso.topeducation_ms3.services;
+
+import com.mingeso.topeducation_ms3.dtos.pago.PagoDTO;
+import com.mingeso.topeducation_ms3.dtos.pago.PagosResponse;
+import com.mingeso.topeducation_ms3.dtos.razon.RazonDTO;
+import com.mingeso.topeducation_ms3.exceptions.ApiErrorException;
+import com.mingeso.topeducation_ms3.exceptions.RegistroNoExisteException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.List;
+
+@Service
+public class PagoService {
+    WebClient pagoWebClient;
+
+    public PagoService(WebClient.Builder webClientBuilder){
+        this.pagoWebClient = webClientBuilder.baseUrl("http://localhost:8080/pagos").build();
+    }
+
+    public List<PagoDTO> obtenerPagos(){
+        PagosResponse response = this.pagoWebClient
+                .get()
+                .retrieve()
+                .bodyToMono(PagosResponse.class)
+                .onErrorResume(WebClientResponseException.class, ex -> {
+                    if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+                        throw new RegistroNoExisteException("Pagos no encontrados.");
+                    } else {
+                        throw new ApiErrorException("Error al obtener los pagos.");
+                    }
+                })
+                .block();
+        if(response == null) throw new ApiErrorException("Error al obtener los pagos.");
+        return response.getPagos();
+    }
+
+}
